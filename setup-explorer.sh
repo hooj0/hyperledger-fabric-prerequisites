7adc12dd6c35db643f34cc32b8fc19c7b735ddca
hoojo <hoojo@qq.com>
2018-08-26 12:19:31 +0800

    :sparkles: :bento: :tada:  Init add Hyperledger blockchain-explorer prerequisites environment software and tools setup script

---
+++ setup-explorer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# --------------------------------------------------------------------
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create date: 2018-08-26
+# copyright by hoojo@2018
+# --------------------------------------------------------------------
+
+#@changelog Hyperledger blockchain-explorer prerequisites environment software and tools setup script
