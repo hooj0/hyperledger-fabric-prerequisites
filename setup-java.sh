15056685ddf4ebcab80ebb63ac8e2d46b1cd1aff
hoojo <hoojo@qq.com>
2018-08-26 11:45:53 +0800

    :sparkles: :bento: :recycle:  Changed java prerequisites environment software and tools setup script

--- setup-java.sh
+++ setup-java.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# --------------------------------------------------------------------
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create date: 2018-08-26
+# copyright by hoojo@2018
+# --------------------------------------------------------------------
+
 #@changelog java prerequisites environment software and tools setup script
 
 
