22e9967b572981ff18b4d84878eabf2da9f3e1cd
hoojo <hoojo@qq.com>
2018-08-28 19:05:52 +0800

    :sparkles: :bento: :recycle:  Changed bootstrap-1 1 sh

--- bootstrap-1.1.sh
+++ bootstrap-1.1.sh
@@ -20,8 +20,8 @@ dockerFabricPull() {
   for IMAGES in peer orderer ccenv javaenv tools; do
       echo "==> FABRIC IMAGE: $IMAGES"
       echo
-      docker pull hyperledger/fabric-$IMAGES:$FABRIC_TAG
-      docker tag hyperledger/fabric-$IMAGES:$FABRIC_TAG hyperledger/fabric-$IMAGES
+      sudo docker pull hyperledger/fabric-$IMAGES:$FABRIC_TAG
+      sudo docker tag hyperledger/fabric-$IMAGES:$FABRIC_TAG hyperledger/fabric-$IMAGES
   done
 }
 
@@ -30,8 +30,8 @@ dockerThirdPartyImagesPull() {
   for IMAGES in couchdb kafka zookeeper; do
       echo "==> THIRDPARTY DOCKER IMAGE: $IMAGES"
       echo
-      docker pull hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG
-      docker tag hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG hyperledger/fabric-$IMAGES
+      sudo docker pull hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG
+      sudo docker tag hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG hyperledger/fabric-$IMAGES
   done
 }
 
@@ -39,33 +39,34 @@ dockerCaPull() {
       local CA_TAG=$1
       echo "==> FABRIC CA IMAGE"
       echo
-      docker pull hyperledger/fabric-ca:$CA_TAG
-      docker tag hyperledger/fabric-ca:$CA_TAG hyperledger/fabric-ca
+      sudo docker pull hyperledger/fabric-ca:$CA_TAG
+      sudo docker tag hyperledger/fabric-ca:$CA_TAG hyperledger/fabric-ca
+}
+
+downloadBinaryFiles() {      
+      echo "===> Downloading platform specific fabric binaries"
+      curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${ARCH}-${VERSION}/hyperledger-fabric-${ARCH}-${VERSION}.tar.gz | tar xz
+
+      echo "===> Downloading platform specific fabric-ca-client binary"
+      curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric-ca/hyperledger-fabric-ca/${ARCH}-${VERSION}/hyperledger-fabric-ca-${ARCH}-${VERSION}.tar.gz | tar xz
+      if [ $? != 0 ]; then
+           echo
+           echo "------> $VERSION fabric-ca-client binary is not available to download  (Avaialble from 1.1.0-rc1) <----"
+           echo
+      fi
+}
+
+dockerHyperledgerImagePull() {
+  log yellow  "===> Pulling fabric Images"
+  dockerFabricPull ${FABRIC_TAG}
+
+  log yellow  "===> Pulling fabric ca Image"
+  dockerCaPull ${CA_TAG}
+
+  log yellow  "===> Pulling thirdparty docker images"
+  dockerThirdPartyImagesPull ${THIRDPARTY_TAG}
 }
 
 : ${CA_TAG:="$MARCH-$CA_VERSION"}
 : ${FABRIC_TAG:="$MARCH-$VERSION"}
 : ${THIRDPARTY_TAG:="$MARCH-$THIRDPARTY_IMAGE_VERSION"}
-
-echo "===> Downloading platform specific fabric binaries"
-curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${ARCH}-${VERSION}/hyperledger-fabric-${ARCH}-${VERSION}.tar.gz | tar xz
-
-echo "===> Downloading platform specific fabric-ca-client binary"
-curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric-ca/hyperledger-fabric-ca/${ARCH}-${VERSION}/hyperledger-fabric-ca-${ARCH}-${VERSION}.tar.gz | tar xz
-if [ $? != 0 ]; then
-     echo
-     echo "------> $VERSION fabric-ca-client binary is not available to download  (Avaialble from 1.1.0-rc1) <----"
-     echo
-fi
-echo "===> Pulling fabric Images"
-dockerFabricPull ${FABRIC_TAG}
-
-echo "===> Pulling fabric ca Image"
-dockerCaPull ${CA_TAG}
-
-echo "===> Pulling thirdparty docker images"
-dockerThirdPartyImagesPull ${THIRDPARTY_TAG}
-
-echo
-echo "===> List out hyperledger docker images"
-docker images | grep hyperledger*
